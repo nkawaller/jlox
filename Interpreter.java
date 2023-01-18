370ee00a2597f5bbbe99cec590441c7a56c3830b
nkawaller <kawaller@gmail.com>
2023-01-17 21:43:57 -0500

    start interpreter

---
+++ Interpreter.java
@@ -0,0 +1,55 @@
+package lox;
+
+class Interpreter implements Expr.Visitor<Object>{
+
+  @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
+
+  @Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    return evaluate(expr.expression);
+  }
+
+  @Override
+  public Object visitUnaryExpr(Expr.Unary expr) {
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case BANG:
+        return !isTruthy(right);
+      case MINUS:
+        return -(double)right;
+    }
+
+    return null;
+  }
+
+  private boolean isTruthy(Object object) {
+    if (object == null) return false;
+    if (object instanceof Boolean) return (boolean)object;
+    return true;
+  }
+
+  private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
+
+  @Override
+  public Object visitBinaryExpr(Expr.Binary expr) {
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case MINUS:
+        return (double)left - (double)right;
+      case SLASH:
+        return (double)left / (double)right;
+      case STAR:
+        return (double)left * (double)right;
+    }
+
+    return null;
+  }
+}
