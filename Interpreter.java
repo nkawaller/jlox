ced1f6ca832c20808867f4fa606d932ed3f5042d
nkawaller <kawaller@gmail.com>
2023-01-28 21:22:13 -0500

    continuing functions

--- Interpreter.java
+++ Interpreter.java
@@ -219,4 +219,27 @@ class Interpreter implements Expr.Visitor<Object>,
 
     return null;
   }
+
+  @Override
+  public Object visitCallExpr(Expr.Call expr) {
+    Object callee = evaluate(expr.callee);
+
+    List<Object> arguments = new ArrayList<>();
+    for (Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+
+    if (!(callee instanceof LoxCallable)) {
+      throw new RuntimeError(expr.paren,
+          "Can only call functions and classes.");
+    }
+
+    LoxCallable function = (LoxCallable)callee;
+    if (arguments.size() != function.arity()) {
+      throw new RuntimeError(expr.paren, "Expected " +
+          function.airity() + " arguments but got " +
+          arguments.size() + ".");
+    }
+    return function.call(this.arguments);
+  }
 }
