094843090744746f144eb4de0c02f03f35bf7a82
nkawaller <kawaller@gmail.com>
2023-02-04 15:45:00 -0500

    finish classes

--- Resolver.java
+++ Resolver.java
@@ -17,8 +17,16 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private enum FunctionType {
     NONE,
     FUNCTION,
+    INITIALIZER,
     METHOD
   }
+
+  private enum ClassType {
+    NONE,
+    CLASS
+  }
+
+  private ClassType currentClass = ClassType.NONE;
 
   void resolve(List<Stmt> statements) {
     for (Stmt statement : statements) {
@@ -36,6 +44,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
+    ClassType enclosingClass = currentClass;
+    currentClass = ClassType.CLASS;
+
     declare(stmt.name);
     define(stmt.name);
 
@@ -44,11 +55,16 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     for (Stmt.Function method : stmt.methods) {
       FunctionType declaration = FunctionType.METHOD;
+      if (method.name.lexeme.equals("init")) {
+        declaration = FunctionType.INITIALIZER;
+      }
+
       resolveFunction(method, declaration);
     }
 
     endScope();
 
+    currentClass = enclosingClass;
     return null;
   }
 
@@ -79,9 +95,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
     
     if (stmt.value != null) {
+      if (currentFunction == FunctionType.INITIALIZER) {
+        Lox.error(stmt.keyword,
+            "Can't return a value from an initializer.");
+      }
       resolve(stmt.value);
     }
-
     return null;
   }
 
@@ -169,6 +188,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitThisExpr(Expr.This expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword,
+          "Can't use 'this' outside of a class.");
+      return null;
+    }
+
     resolveLocal(expr, expr.keyword);
     return null;
   }
