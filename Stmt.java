e1c8f8bfaf451a4259645e3c3ec7b4222c091767
nkawaller <kawaller@gmail.com>
2023-02-06 19:07:04 -0500

    fix line length

--- Stmt.java
+++ Stmt.java
@@ -29,7 +29,8 @@ abstract class Stmt {
   }
 
  static class Class extends Stmt {
-   Class(Token name, Expr.Variable superclass, List<Stmt.Function> methods) {
+   Class(Token name, Expr.Variable superclass, 
+         List<Stmt.Function> methods) {
     this.name = name;
     this.superclass = superclass;
     this.methods = methods;
